f3bf769f16e32b7f28445adfa5573aa5d882eed4
Matias Cabrera <88689998+MatiasCab@users.noreply.github.com>
2023-09-02 01:04:22 -0300

    refactored 2

--- Proyecto 1/main.py
+++ Proyecto 1/main.py
@@ -12,21 +12,20 @@ def emotion_vector(word, vector_s):
         vector_s[1] += 1
         
 # Define el score sentimental (Version 2)
-def emotion_vector_2(vector_s, dic_words):
-    for word in dic_words:
+def emotion_vector_2(vector_s, keywords, vector):
+    for i in range(len(keywords)):
+        word = keywords[i]
         if word in keywords_positive:
-            vector_s[0] += dic_words[word]
+            vector_s[0] += vector[i]
         elif word in keywords_negative:
-            vector_s[2] += dic_words[word]
+            vector_s[2] += vector[i]
         elif word in keywords_neutral:
-            vector_s[1] += dic_words[word]
+            vector_s[1] += vector[i]
 
 # Calcula el promedio del sentimiento de cada tweet
-def avg_vector(vector_s):
-    avg = np.mean(vector_s)
-    #avg_rounded = round(avg, 2)
-    return avg
-    
+def avg_vector(vector):
+    return np.array([round(i / len(vector), 2) for i in vector])
+
 # Limpia el tweet, sacando tildes, signos y mayúsculas
 def clean_tweet(tweet):
     cleaned_tweet = ud.unidecode(tweet.lower())
@@ -43,8 +42,8 @@ def score(vector_s):
 
 def key_words_mapper(word, keywords, vector):
     for i in range(len(keywords)):
-        if word is keywords[i]:
-            vector[i] += vector[i]
+        if word == keywords[i]:
+            vector[i] += 1
             return
 
 tweets = [
@@ -54,13 +53,24 @@ tweets = [
     "Mi día fue un desastre total. Nada salió como lo planeé.",
 ]
 
+#Sacarrrr
+tweets = [
+    "Que desprecio que me genera el gobierno de Fernandez! Es impresionante la inflacion que se genero. Es un muerto! Aun con trabajo no podes vivir!",
+    "Estoy muy feliz, al fin me recibi de Ingeniero en Informatica, que orgullo!",
+    "Ayer me estaba comiendo un sanguche de bondiola, no saben lo orgulloso que estoy del buen trabajo del de la fiambreria. Este sanguche es un exito.",
+    "Mi día fue un desastre total. Nada salió como lo planeé.",
+]
+
 keywords_positive = ["excelente", "inspiradores", "increible"]
 keywords_negative = ["triste", "fallecimiento", "perdida", "problemas", "desastre"]
 keywords_neutral = ["noticia", "creer", "presentacion", "noche", "musica"]
-keywords = keywords_negative + keywords_neutral + keywords_positive
 
-# Inicializamos el diccionario con todas las palabras en 0
-vector = np.array([0 for i in range(len(keywords))])
+#sacarrr
+keywords_positive = ["feliz", "orgullo", "exito"]
+keywords_negative = ["muerto", "desprecio", "inflacion"]
+keywords_neutral = ["trabajo", "bondiola", "sanguche"]
+
+keywords = keywords_positive + keywords_neutral + keywords_negative
 
 # Inicialización para hacer un seguimiento del tweet más positivo y negativo
 tweet_more_positive = None
@@ -70,13 +80,13 @@ min_score = float('inf')
 result_mean_quality = []
 
 for tweet in tweets:
+    vector = np.array([0 for i in range(len(keywords))])
     vector_s = np.array([0, 0, 0])
     for word in clean_tweet(tweet):
         key_words_mapper(word, keywords, vector)
-    emotion_vector_2(vector_s, vector)
-    result_quality = avg_vector(vector)
-    result_mean_quality.append(result_quality)
-    feelings_mean = avg_vector(vector_s)
+    emotion_vector_2(vector_s, keywords, vector)
+    avereged_vector = avg_vector(vector)
+    avereged_feelings_vector = avg_vector(vector_s)
     tweet_score = score(vector_s)
 
     if tweet_score > max_score:
@@ -88,11 +98,11 @@ for tweet in tweets:
 
     print("Tweet:", tweet)
     print("Vector s:", vector_s)
-    print("Promedio del sentimiento:", tweet_score)
-    print("La calidad del tweet:", result_quality)
+    print("Promedio del sentimiento:", avereged_feelings_vector)
+    print("La calidad del tweet:", avereged_vector)
     print("El score es:", tweet_score)
     print("---------------------------------------------\n")
 
 print("El tweet más positivo es:", tweet_more_positive)
 print("El tweet más negativo es:", tweet_more_negative)
-print("La calidad promedio es de:", np.mean(result_mean_quality)) #No estoy muy seguro de esto.
+#print("La calidad promedio es de:", np.mean(result_mean_quality)) #No estoy muy seguro de esto.
