b4aa2b2276a8c8fbb17d140f48136bbb703c34bf
Matias Cabrera <88689998+MatiasCab@users.noreply.github.com>
2023-10-26 10:45:43 -0300

    Project 4 - equation system maker

--- Proyecto 4/proyecto_4.py
+++ Proyecto 4/proyecto_4.py
@@ -1,49 +1,28 @@
 import numpy as np
 
-def lights_out_solver(initial_state):
-    n = len(initial_state)
-    
-    # Función para verificar si el tablero está apagado
-    def is_board_off(board):
-        return all(all(cell == 0 for cell in row) for row in board)
-    
-    # Función para cambiar el estado de las luces adyacentes
-    def toggle_lights(board, row, col):
-        directions = [(0, 0), (-1, 0), (1, 0), (0, -1), (0, 1)]
-        for dr, dc in directions:
-            new_row, new_col = row + dr, col + dc
-            if 0 <= new_row < n and 0 <= new_col < n:
-                board[new_row][new_col] = 1 - board[new_row][new_col]
-    
-    # Función de retroceso para encontrar la solución
-    def backtrack(board, row, col):
-        if row == n:
-            return is_board_off(board), []
-        
-        next_row, next_col = row, col + 1
-        if next_col == n:
-            next_row, next_col = row + 1, 0
-        
-        no_press, no_lights = backtrack(board.copy(), next_row, next_col)
-        toggle_lights(board, row, col)
-        yes_press, yes_lights = backtrack(board.copy(), next_row, next_col)
-        if yes_press:
-            yes_lights.append((row, col))
-            return yes_press, yes_lights
-        return no_press, no_lights
-    
-    _, lights_to_press = backtrack(initial_state.copy(), 0, 0)
-    
-    # Genera vector solución
-    solution = [1 if (i // n, i % n) in lights_to_press else 0 for i in range(n**2)]
-    
-    return solution, lights_to_press
+A = np.array([[0, 1, 0],
+              [1, 1, 0],
+              [0, 0, 1]])
 
-# Ejemplo:
-initial_state = np.array([[0, 1, 0],
-                          [1, 0, 0],
-                          [1, 0, 1]])
-solution, lights_to_press = lights_out_solver(initial_state)
 
-print("Vector solución:", solution)
-print("Luces a presionar:", lights_to_press)
+def get_equation_vector(matrix, x, y):
+    matrix_size = len(matrix)
+    zero_matrix = np.zeros((matrix_size, matrix_size))
+    possible_changes = [(x, y), (x + 1, y), (x, y + 1), (x - 1, y), (x, y - 1)]
+    for entry in possible_changes:
+        entry_x = entry[0]
+        entry_y = entry[1]
+        if len(matrix[0]) > entry_x >= 0 and len(matrix) > entry_y >= 0:
+            zero_matrix[entry_y][entry_x] = 1
+    equation_vector = zero_matrix.ravel()
+    return np.append(equation_vector, matrix[y][x])
+
+
+equation_system = None
+for index_y, B in enumerate(A):
+    for index_x, _ in enumerate(B):
+        if equation_system is None:
+            equation_system = get_equation_vector(A, index_x, index_y)
+        else:
+            equation_system = np.vstack((equation_system, get_equation_vector(A, index_x, index_y)))
+print(equation_system)
