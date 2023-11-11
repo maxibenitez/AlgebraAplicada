181c651c00a955635df44218e7935a815fd0ce68
Matias Cabrera <88689998+MatiasCab@users.noreply.github.com>
2023-11-10 23:54:29 -0300

    Added user interface

--- Proyecto 3/proyecto_3.py
+++ Proyecto 3/proyecto_3.py
@@ -1,4 +1,7 @@
 import numpy as np
+import os
+import sys
+
 
 def get_equation_vector(matrix, x, y):
     matrix_size = len(matrix)
@@ -51,15 +54,85 @@ def back_substitution(matrix):
         x[i] = res
     return x
 
-def lights_out_solver(initial_state):
-    equation_system = get_equation_system(initial_state)
+
+def system_have_solution(equation_system):
+    row_length, columns = np.shape(equation_system)
+    for i in range(row_length):
+        result = equation_system[i][columns - 1]
+        if result == 1:
+            have_one = False
+            for j in range(columns - 1):
+                if equation_system[i][j] == 1:
+                    have_one = True
+                    break
+            if not have_one:
+                sys.exit("\n#############¡La Matriz ingresada no tiene solución!#############\n")
+
+
+
+
+def lights_out_solver(initial_matrix):
+    equation_system = get_equation_system(initial_matrix)
     triangular_matrix = forward_elimination(equation_system)
+    system_have_solution(triangular_matrix)
     solution_vector = back_substitution(triangular_matrix)
     return solution_vector
 
-# Ejemplo
-initial_state = np.array([[0, 1, 0],
-                        [1, 1, 0],
-                        [0, 0, 1]])
-solution = lights_out_solver(initial_state)
-print("Vector solución:", solution)
+
+def clear_console():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+
+def placeholder_matrix(matrix, row_position):
+    clear_console()
+    print("Matriz ingresada:\n")
+    for i in range(len(matrix)):
+        actual_row = matrix[i]
+        if i == row_position:
+            actual_row = f"> {actual_row} <"
+        print(actual_row)
+
+
+def charge_input_matrix():
+    matrix_size = int(input("\nIngrese el tamaño del tablero: "))
+    charged_matrix = np.full((matrix_size, matrix_size), "x")
+    for i in range(matrix_size):
+        for j in range(matrix_size):
+            element = -1
+            while element not in ["0", "1"]:
+                placeholder_matrix(charged_matrix, i)
+                element = input(f"\nIngrese el estado de la  (1 = encendidad 0 = apagada) en la posición {i} {j}: ")
+
+            charged_matrix[i][j] = element
+            clear_console()
+    return charged_matrix
+
+
+def matrix_logger(matrix):
+    for line in matrix:
+        print(line)
+
+
+def solver_logger(initial_game, solution_vector):
+    matrix_size = len(initial_game)
+    matrix_solution = solution_vector.reshape((matrix_size, matrix_size))
+
+    print("\nMatriz solución:\n")
+    matrix_logger(matrix_solution)
+    print("\nVector solución:", solution_vector)
+
+
+def start_game_solution():
+    clear_console()
+    input_matrix = charge_input_matrix().astype(int)
+    print("¡Matriz ingresada con éxito!\n")
+    print("Matriz final ingresada:\n")
+    matrix_logger(input_matrix)
+    start_game = input("\nDesea encontrar la solución de esta matriz? (y = si otro = no): ")
+    if start_game != "y":
+        sys.exit()
+    solution = lights_out_solver(input_matrix)
+    solver_logger(input_matrix, solution)
+
+
+start_game_solution()
